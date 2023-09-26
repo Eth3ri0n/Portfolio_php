0d2f157e36874fef1d51a82056f196cd7573a7cd
Eth3ri0n <eth3rias@gmail.com>
2023-09-26 10:30:43 +0200

    Init repository

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="Ceci est un portfolio pédagogique réaliser à L'IUT de Tarbes dans le Département Métiers du Multimédia et de l'Internet ">
+    <meta name="authors" content="Enzo Borel, Thierryno Randrianarisaina">
+    <title>Portfolio | Enzo Borel - Thierryno Randrianarisaina</title>
+</head>
+
+<body>
+    <h1>Portfolio</h1>
+</body>
+
+</html>
